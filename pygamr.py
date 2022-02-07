bb880ae73cb5b891ad6393a9572fd36a5b88d556
tristan0312 <ardeventristan@gmail.com>
2022-02-07 11:43:11 +0100

    Create pygamr.py

---
+++ pygamr.py
@@ -0,0 +1,19 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+fenetre.fill([10,186,181])
+
+perso = pygame.image.load("perso.png").convert_alpha()
+position_perso = perso.get_rect()
+
+fenetre.blit(perso, position_perso)
+
+pygame.display.flip()
+
+while True :
+  pass
+
+
