e78feafd47b3a736e02262caeb4a6f4805830e90
tristan0312 <99179072+tristan0312@users.noreply.github.com>
2022-05-02 11:44:36 +0200

    Update pygamr.py

--- pygamr.py
+++ pygamr.py
@@ -1,45 +1,13 @@
-import pygame, sys
-from pygame.locals import *
-import time
-
-pygame.init()
-pygame.key.set_repeat(50)
-
-fenetre = pygame.display.set_mode((1250, 1000))
-fenetre.fill([204,204,255])
-cpt = 0
-chrono_record = 0
-font = pygame.font.Font(pygame.font.get_default_font(), 30)
-text = font.render("le but est de faire 100 clic avec la fleche du haut le plus rapidement possible ", True, (0, 0, 0))
-fenetre.blit(text, dest=(0,50))
-
-
-while True :
-    top = time.time()
-    for event in pygame.event.get() :    
-        if event.type == KEYDOWN:
-            if event.key == K_UP :
-                cpt = cpt + 1
-                if cpt == 100:
-                    font = pygame.font.Font(pygame.font.get_default_font(), 50)
-                    text = font.render("100 clic on etait efectué", True, (76, 0, 153))
-                    fenetre.blit(text, dest=(400,400))
-                    chrono_parti = time.time() - top
-                    if chrono_parti > chrono_record:
-                        chrono_record = chrono_parti
-                        print("ton record actuelle est de",chrono_record)
-                    else:
-                        print("ton record actuelle est de",chrono_record)
-                    
-                        
-                        
-
-                if event.type == pygame.QUIT:
-                    pygame.display.quit()
-                    sys.exit()
-    
-                pygame.display.flip()
-
+lst = ['Emmy', 'Ahmed', 'Antoine', 'Emma', 'Louan', 'Louka', 'Louan', 'Elouo', 'Candice', 'Tristan', 'Alissa', 'Louka', 'Louka', 'Emma', 'Alissa', 'Antoine', 'Elouo', 'Louan', 'Mathias', 'Candice', 'Antoine', 'Tristan', 'Louka', 'Emma', 'Tristan', 'Mathias', 'Louan', 'Vincent', 'Vincent', 'Ahmed', 'Louka', 'Elouo', 'Ahmed', 'Elouo', 'Candice', 'Louka', 'Mathias', 'Mathias', 'Emmy', 'Candice', 'Tristan', 'Antoine', 'Mathias', 'Ahmed', 'Candice', 'Louka', 'Alissa', 'Vincent', 'Elouo', 'Vincent', 'Antoine', 'Elouo', 'Emmy', 'Hugo', 'Vincent', 'Louan', 'Emmy', 'Emma', 'Vincent', 'Louan', 'Elouo', 'Emmy', 'Emmy', 'Emma', 'Mathias', 'Elouo', 'Louka', 'Hugo', 'Emma', 'Vincent', 'Candice', 'Ahmed', 'Hugo', 'Emma', 'Candice', 'Louan', 'Louka', 'Candice', 'Emma', 'Hugo', 'Mathias', 'Tristan', 'Mathias', 'Tristan', 'Antoine', 'Antoine', 'Hugo', 'Louka', 'Mathias', 'Hugo', 'Alissa', 'Elouo', 'Louka', 'Louka', 'Alissa', 'Vincent', 'Ahmed', 'Emma', 'Hugo', 'Mathias', 'Hugo', 'Mathias', 'Antoine', 'Ahmed', 'Antoine', 'Antoine', 'Vincent', 'Ahmed', 'Candice', 'Mathias', 'Emmy', 'Mathias', 'Emma', 'Antoine', 'Tristan', 'Antoine', 'Tristan', 'Candice', 'Louka', 'Louka', 'Tristan', 'Ahmed', 'Elouo', 'Emmy', 'Antoine', 'Alissa', 'Ahmed', 'Antoine', 'Alissa', 'Louan', 'Emma', 'Elouo', 'Tristan', 'Emmy', 'Elouo', 'Louka', 'Emmy', 'Ahmed', 'Louka', 'Vincent', 'Vincent', 'Antoine', 'Ahmed', 'Ahmed', 'Vincent', 'Tristan', 'Louan', 'Emmy', 'Elouo', 'Louka', 'Emmy', 'Hugo', 'Emmy', 'Emma', 'Emma', 'Hugo', 'Louan', 'Hugo', 'Antoine', 'Ahmed', 'Emmy', 'Vincent', 'Emma', 'Candice', 'Candice', 'Louka', 'Louan', 'Alissa', 'Vincent', 'Vincent', 'Tristan', 'Louka', 'Louan', 'Louka', 'Antoine', 'Tristan', 'Alissa', 'Ahmed', 'Vincent', 'Louka', 'Alissa', 'Mathias', 'Elouo', 'Emmy', 'Ahmed', 'Emmy', 'Vincent', 'Elouo', 'Emma', 'Mathias', 'Ahmed', 'Vincent', 'Mathias', 'Candice', 'Elouo', 'Louan', 'Elouo', 'Vincent', 'Emmy', 'Emma']    
+apparition = {}
+for prenom in lst:
+    if prenom not in apparition:
+        apparition[prenom] = 1
+    else:
+        apparition[prenom]+=1
+        
+print(apparition)
+        
 
 
 
